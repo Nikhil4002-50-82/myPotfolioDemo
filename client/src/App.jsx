2c2f88837dc1a17a8eca88f381237a2ec5f00ec3
Nikhil <nikhil.4002.50.82@gmail.com>
2025-02-08 21:22:40 +0530

    addded another timeline

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,10 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-  const cursor =useRef();
+  // const cursor =useRef();
   const menu=useRef();
   const tl=useRef(null);
+  const t2=useRef(null)
   const closeIcon =useRef();
 
   useGSAP(()=>{  
@@ -68,7 +69,6 @@ const App = () => {
         trigger:".screen",
         scroller:"body",
         toggleActions:"play none none reverse",
-        markers:true,
         start:"top center"
       }
     })
@@ -79,6 +79,29 @@ const App = () => {
         scrub:true,
         pin:true,
       }
+    })
+  
+    t2.current=gsap.timeline({
+      scrollTrigger:{
+        trigger:".screen",
+        scroller:"body",
+        toggleActions:"play none none reverse",
+        start:"top top",
+        markers:true,
+        pin:true
+      }
+    })
+
+    t2.current.from(".about",{
+      y:-100,
+      opacity:0,
+      // duration:0.5,
+      stagger:0.1,
+    })
+    t2.current.from(".name",{
+      y:100,
+      opacity:0,
+      duration:0.5,
     })
     
     // document.addEventListener("mousemove",(event)=>{
@@ -92,17 +115,17 @@ const App = () => {
   })
 
   return (
-    <div className='bg-[#141611] text-white w-[100vw] h-[auto] overflow-x-hidden bg-cover bg-center'>
+    <div className='bg-[#98e12c] text-black w-[100vw] h-[auto] overflow-x-hidden'>
       {/* <div ref={cursor} className='h-[2em] w-[2em] rounded-3xl bg-white fixed z-30'></div> */}
       
-      <nav className='p-[3em] pt-[1.5em] pb-[1.5em] w-[100vw] fixed flex justify-between z-40'>
+      <nav className='p-[3em] pt-[1.5em] pb-[1.5em] w-[100%] fixed flex justify-between z-40'>
         <h1 className='text-4xl font-bold cursor-pointer'>PoRtFoLiO</h1>
         <div className='h-[2em] z-50' onClick={()=>{
           tl.current.play();
         }}>< AiOutlineMenuFold className='h-[100%] w-[100%]' /></div>
       </nav>
 
-      <div ref={menu} className='fixed w-[30%] h-[100%] bg-white/30 backdrop-blur-sm top-0 right-0 text-3xl p-[2em] z-50'>
+      <div ref={menu} className='fixed w-[30%] h-[100%] bg-white/30 backdrop-blur-sm top-0 right-0 text-3xl font-bold p-[2em] z-50'>
         <div ref={closeIcon} className='fixed h-[1em] w-[1em] top-0 right-0 p-[1em] pr-[2em]' onClick={()=>{
           tl.current.reverse();
         }}><ImCancelCircle/></div>
@@ -113,7 +136,7 @@ const App = () => {
         <h1 className='mb-[2em] h1'>My Contact</h1>
       </div>
 
-      <div className='h-[80vh] w-[100vw] bg-opacity-[2em] bg-[url("https://images.unsplash.com/photo-1439792675105-701e6a4ab6f0?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-cover bg-center flex items-center justify-center font-bold img-container overflow-hidden'>
+      <div className='h-[100vh] w-[100vw] bg-opacity-[2em] flex items-center justify-center font-bold img-container overflow-hidden'>
         <h1 className='text-[10em] overflow-hidden'>
           <span className='left inline-block'>D</span>
           <span className='left inline-block'>E</span>
@@ -129,8 +152,18 @@ const App = () => {
         </h1>
       </div>
 
-      <div className='w-[100vw] h-[200vh] '>
-        <div className='bg-white h-[20px] w-[20px] screen'></div>
+      <div className='w-[100vw] h-[auto] '>
+        <div className='h-[50px] w-[50px] screen relative top-0 left-0'></div>
+        <div className='w-full h-[60vh] flex gap-10 p-[1em]'>
+          <div className='bg-white/30 backdrop-blur-sm w-[25%] h-full about'></div>
+          <div className='bg-white/30 backdrop-blur-sm w-[25%] h-[85%] about'></div>
+          <div className='bg-white/30 backdrop-blur-sm w-[25%] h-[70%] about'></div>
+          <div className='bg-white/30 backdrop-blur-sm w-[25%] h-[50%] about'></div>
+        </div>
+        <div className=' p-[1em] pt-0 pb-0 name'>
+          <span className='text-[10em] font-extrabold'>DEVELOPER</span>
+          <span className='text-[2em] font-bold'>Nikhil R Nambiar</span>
+        </div>
       </div>
     </div>
   )
